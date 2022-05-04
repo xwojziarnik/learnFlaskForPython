a3fa1bee720416a4f9c25ee61e11b0ebb527d430
xwojziarnik <woj.ziarnik@gmail.com>
2022-05-04 23:47:58 +0200

    Day010 - done. I'm in a halfway of freeCodeCamp tutorial.

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, redirect, render_template, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+
+from requests import request
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
@@ -17,9 +19,22 @@ class Todo(db.Model):
         return '<Task %r>' % self.id
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding your task'
+
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
 
 
 if __name__ == '__main__':
