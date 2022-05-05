eaad6fc703b9813a85cf030f8d368f12189a1ce0
xwojziarnik <woj.ziarnik@gmail.com>
2022-05-05 08:58:09 +0200

    Day011 - done. Created two endpoints and started creating third.

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, redirect, render_template, url_for, request
+from crypt import methods
+import flask
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-
 from requests import request
+import requests
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
@@ -21,20 +22,32 @@ class Todo(db.Model):
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    if request.method == 'POST':
-        task_content = request.form['content']
+    if flask.request.method == 'POST':
+        task_content = flask.request.form['content']
         new_task = Todo(content=task_content)
 
         try:
             db.session.add(new_task)
             db.session.commit()
-            return redirect('/')
+            return flask.redirect('/')
         except:
-            return 'There was an issue adding your task'
+            return "There was an issue adding your task."
 
     else:
         tasks = Todo.query.order_by(Todo.date_created).all()
-        return render_template('index.html', tasks=tasks)
+        return flask.render_template('index.html', tasks=tasks)
+
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return flask.redirect('/')
+    except:
+        return 'There was a problem deleting that task.'
 
 
 if __name__ == '__main__':
